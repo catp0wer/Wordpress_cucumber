5963d520f1bc5fde486e08e8460d01ae4c9c1f82
catp0wer <catp0wer@mail.ru>
2018-11-09 19:10:19 +0200

    adjusted according to new added clases and methods

--- src/main/java/stepDefinitions/UISteps.java
+++ src/main/java/stepDefinitions/UISteps.java
@@ -1,57 +1,58 @@
 package stepDefinitions;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import dataProviders.ConfigFileReader;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import pages.LoginPage;
 import pages.ToolBarPage;
 
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
 
 
 public class UISteps {
+    WebDriver driver;
+    LoginPage loginPage;
+    ToolBarPage homePage;
+    ConfigFileReader fileReader;
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
 
-    WebDriver driver = null;
+        loginPage = new LoginPage(driver);
+        PageFactory.initElements(driver, loginPage);
+
+        homePage = new ToolBarPage(driver);
+        PageFactory.initElements(driver, homePage);
+
+        fileReader = new ConfigFileReader();
+    }
+
 
     @Given("^user navigates to Login page$")
-
     public void userNavigatesToPage() {
-        driver = new FirefoxDriver();
-        driver.navigate().to("https://catp0wer150470290.wordpress.com/wp-admin/");
+        loginPage.open();
     }
 
     @When("^user logs in with '(.*)' username and '(.*)' password$")
     public void userLogsInWithUsername(String name, String password) {
-        LoginPage lg=new LoginPage();
-        PageFactory.initElements(driver, lg);
-        lg.userLogin(name,password);
+        loginPage.userLogin(name,password,driver);
     }
-//    @Given("^user opens wordpress$")
-//
-//    public void userNavigatesToPage() {
-//        driver = new FirefoxDriver();
-//        driver.navigate().to("https://catp0wer150470290.wordpress.com/wp-admin/");
-//    }
-//    @When("^user hover over Posts and clicks on Add$")
-//    public void userClickAdd() {
-//        System.out.println("user clicks on Add");
-//    }
-//    @And("^user enter title$")
-//    public void userEnterTitle() {
-//        System.out.println("user clicks on Add");
-//    }
-//    @And("^user click publish$")
-//    public void userClicPublish() {
-//        System.out.println("user clicks on Add");
-//    }
-//
-//    @Then("^new post is added$")
-//    public void newPostisAdded() {
-//        System.out.println("new post is added");
-//
-//    }
+
+    @Then("^user is redirected to HomePage$")
+    public void homeElementIsPresent(){
+        homePage.isElementPresent(driver,homePage.expectedDashboardText,homePage.dashboardTitle);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }
