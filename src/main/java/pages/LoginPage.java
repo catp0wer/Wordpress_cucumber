8a15eb6dbed5b6c8d7ab831119683574023a85f0
catp0wer <catp0wer@mail.ru>
2018-11-09 19:01:01 +0200

    did refactoring for Login method and added a new method open()

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,9 +1,25 @@
 package pages;
 
+import dataProviders.ConfigFileReader;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class LoginPage {
+import java.io.IOException;
+
+public class LoginPage extends PageObject{
+    private PageObject pageObject = new PageObject();
+    private ConfigFileReader fileReader= new ConfigFileReader();
+    private WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+
+    }
 
     @FindBy(xpath = "//button[contains(@class,'button form-button is-primary')]")
     private WebElement continue_login;
@@ -14,17 +30,23 @@ public class LoginPage {
     @FindBy(xpath = "//input[@id='password']")
     private WebElement userPassword;
 
-//    public WebElement continue_login() {
-//        return continue_login;
-//    }
+    public void open(){
+        try {
+            driver.navigate().to(fileReader.getProp("URL"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    // toolbarpage?
-    public void userLogin(String username, String password) {
+    public void userLogin(String username, String password,WebDriver driver) {
+        //wait until the element is visible
+        pageObject.onThePage(userName,driver);
         userName.sendKeys(username);
         continue_login.click();
+        //wait until the element is visible
+        pageObject.onThePage(userPassword,driver);
         userPassword.sendKeys(password);
         continue_login.click();
-        //return new ToolBarPage();
     }
 }
 
