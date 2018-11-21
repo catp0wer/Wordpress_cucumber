05ceccb4d22422cb967bf4071813a62a2e6175f8
catp0wer <catp0wer@mail.ru>
2018-11-22 00:15:20 +0200

    added new pom page for post

---
+++ src/main/java/pages/PostPage.java
@@ -0,0 +1,39 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.junit.Assert.assertEquals;
+
+public class PostPage extends PageObject {
+
+    private PageObject pageObject = new PageObject();
+    private WebDriver driver;
+    public PostPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath ="//input[@id='title']")
+    public WebElement postTitle;
+
+    @FindBy(xpath = "//input[@id='publish']")
+    public WebElement publishButton;
+
+    @FindBy(xpath = "//div[@id='message']/p")
+    public WebElement postPublished;
+
+    @FindBy(xpath = "//span[@id='post-status-display']")
+    public WebElement postStatus;
+
+    public void enterTitle(){
+        postTitle.sendKeys("Writing title");
+    }
+    public String expectedPostPublishedText = "Published";
+    public void isElementPresent(WebDriver driver, String expectedText, WebElement element){
+        pageObject.onThePage(element,driver);
+        assertEquals(expectedText, element.getText());
+    }
+}
