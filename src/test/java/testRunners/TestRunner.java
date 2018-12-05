5ef7b3b585000ebfaf20065b2e5f0072ed03ed3e
catp0wer <catp0wer@mail.ru>
2018-12-05 23:52:41 +0200

    changed properties for maven and version for junit in pom, renamed testRunner class to TestRunner

--- src/test/java/testRunners/TestRunner.java
+++ src/test/java/testRunners/TestRunner.java
@@ -9,7 +9,9 @@ import cucumber.api.junit.Cucumber;
 
 @CucumberOptions(features="src/test/resources/features/EditPost.feature", glue="stepDefinitions")
 
-
-public class testRunner {
+//NOTE: Maven test autodiscovery will only look for classes that follow one of these patterns:
+//Test* or *Test, and it is case sensitive. So `testRunner` wouldn't work, and you'll have no hint
+//about why that is the case
+public class TestRunner {
 
 }
